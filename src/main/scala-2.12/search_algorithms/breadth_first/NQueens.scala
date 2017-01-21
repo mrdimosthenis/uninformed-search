ca5d8c12f54277426efa460d07c52bd9174ff751
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-01-21 15:48:14 +0200

    use immutable queue

--- src/main/scala-2.12/search_algorithms/breadth_first/NQueens.scala
+++ src/main/scala-2.12/search_algorithms/breadth_first/NQueens.scala
@@ -1,19 +1,17 @@
 package search_algorithms.breadth_first
 
+import scala.collection.immutable.Queue
+
 object NQueens extends App{
 
   val n = 4
 
-  var queue = List.empty[Vector[Int]]
-  queue = Vector.empty[Int] :: queue
+  var queue = Queue(Vector.empty[Int])
   while (queue.nonEmpty){
-    val children = (0 until n).map(i => queue.last :+ i)
-    queue = queue.dropRight(1)
-    children.foreach(ch => {
-      queue = ch :: queue
-      Thread.sleep(500)
-      println(ch)
-    })
+    queue = queue.enqueue((0 until n).map(i => queue.dequeue._1 :+ i))
+    queue = queue.dequeue._2
+    Thread.sleep(500)
+    println(queue)
   }
 
 }
