97e58395a127cf7c01913df0b0481304e4d32cb5
mrdimosthenis <mrdimosthenis@hotmail.com>
2017-03-18 21:36:20 +0200

    delete useless stuff

--- src/main/scala-2.12/Algorithms.scala
+++ src/main/scala-2.12/Algorithms.scala
@@ -1,7 +1,6 @@
 import models.{Node, TrackNode}
 
 import scala.annotation.tailrec
-import scala.collection.immutable.Queue
 
 object Algorithms {
 
@@ -31,8 +30,6 @@ object Algorithms {
 
   }
 
-  //
-
   def withTracking[A, B](startingNode: TrackNode[A, B], isBreadthFirst: Boolean = true): Option[Vector[B]] = {
 
     @tailrec
@@ -56,38 +53,6 @@ object Algorithms {
 
     val startingSeq = if (isBreadthFirst) Vector(startingNode) else List(startingNode)
     recur(startingSeq, Set.empty[A])
-
-  }
-
-  def breadthFirstWithTracking[A, B](startingNode: TrackNode[A, B]): Option[Vector[B]] = {
-
-    @tailrec
-    def recur(queue: Queue[TrackNode[A, B]], trackSet: Set[A]): Option[Vector[B]] = {
-      if (queue.isEmpty) None
-      else {
-        val currentNode = queue.dequeue._1
-        if (currentNode.isSolution) Some(currentNode.path)
-        else recur(queue.dequeue._2.enqueue(currentNode.neighbors(trackSet)), trackSet + currentNode.content)
-      }
-    }
-
-    recur(Queue(startingNode), Set.empty[A])
-
-  }
-
-  def depthFirstWithTracking[A, B](startingNode: TrackNode[A, B]): Option[Vector[B]] = {
-
-    @tailrec
-    def recur(stack: List[TrackNode[A, B]], trackSet: Set[A]): Option[Vector[B]] = {
-      if (stack.isEmpty) None
-      else {
-        val currentNode = stack.head
-        if (currentNode.isSolution) Some(currentNode.path)
-        else recur(currentNode.neighbors(trackSet).toList ++ stack.tail, trackSet + currentNode.content)
-      }
-    }
-
-    recur(List(startingNode), Set.empty[A])
 
   }
 
